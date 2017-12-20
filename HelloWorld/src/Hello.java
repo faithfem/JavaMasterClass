055c541804c2abc3736ac66151c1ebe351947a4a
faithfem <fmugure@intracitygeeks.org>
2017-12-20 00:19:23 -0500

    variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,16 @@
 public class Hello {
     public static void main(String[]args) {
         System.out.println("Hello, World!");
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = (10+5) + (2*10);
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myFirstNumber);
+        System.out.println(myThirdNumber);
+        System.out.println(mySecondNumber + myThirdNumber);
+        System.out.println(myTotal);
+
+
     }
 }
