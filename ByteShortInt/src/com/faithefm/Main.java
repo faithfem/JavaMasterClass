a2a785617a9b1262636d0f0e79e9be6f366179bb
faithfem <fmugure@intracitygeeks.org>
2017-12-20 21:31:50 -0500

    Sec4 Lec 22 More on Operators & Precedence

--- ByteShortInt/src/com/faithefm/Main.java
+++ ByteShortInt/src/com/faithefm/Main.java
@@ -22,5 +22,47 @@ public class Main {
 
         System.out.println(myMinValue);
         System.out.println(myByteValue);
+
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " + result);
+
+        //1. create a double variable with the value 20
+        //2. create a second variable of type double with the value 80
+        //3. add both numbers up and multiply by 25
+        //4. use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+        //5. write an "if" statement that displays a message "Total was over the limit"
+        //   if the remaining total in #4 is equal to 20 or less.
+
+        double myFirstNumber = 20;
+        double mySecondNumber = 80;
+        double myResult = (myFirstNumber + mySecondNumber) * 25;
+        double myRemainder = myResult%40;
+        System.out.println("My Result is " + myResult);
+        System.out.println("The Remainder is " + myRemainder);
+        if(myRemainder <= 20){
+            System.out.println("Total was over the limit!");
+        } else{
+            System.out.println("Total was NOT over the limit!");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
