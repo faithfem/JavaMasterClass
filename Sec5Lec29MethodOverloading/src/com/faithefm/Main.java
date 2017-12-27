41e1baf6842aded908e1004aecd4d8971792d9e7
faithfem <fmugure@intracitygeeks.org>
2017-12-26 22:01:10 -0500

    Sec5 Lec29 MethodOverloading

--- Sec5Lec29MethodOverloading/src/com/faithefm/Main.java
+++ Sec5Lec29MethodOverloading/src/com/faithefm/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// method overloading: same method name but diff parameters
+    calculateScore("Tim", 500);
+
     }
+    //method below is created, then the method (called calculateScore) is used above
     public static int calculateScore(String playerName, int score){
-        System.out.println("Player" + playerName + " scored " + score + "points");
+        System.out.println("Player " + playerName + " scored " + score + " points");
         return score * 1000;
     }
 }
