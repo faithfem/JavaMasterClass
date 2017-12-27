fc78118f8fab9d2c9b0bbbba1c1037ff3944bef2
faithfem <fmugure@intracitygeeks.org>
2017-12-26 22:07:17 -0500

    Sec5 Lec29 MethodOverloading

--- Sec5Lec29MethodOverloading/src/com/faithefm/Main.java
+++ Sec5Lec29MethodOverloading/src/com/faithefm/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// method overloading: same method name but diff parameters
-    calculateScore("Tim", 500);
+    int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
 
     }
     //method below is created, then the method (called calculateScore) is used above
