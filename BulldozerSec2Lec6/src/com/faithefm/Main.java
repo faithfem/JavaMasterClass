b4d9a6247b7524c0866a167425f35cfdab5b91f6
faithfem <fmugure@intracitygeeks.org>
2018-01-08 22:19:06 -0500

    OOPSec2Lec6

---
+++ BulldozerSec2Lec6/src/com/faithefm/Main.java
@@ -0,0 +1,19 @@
+package com.faithefm;
+
+import java.io.Console;
+
+public class Main { //Object Oriented programming use keyword class a lot
+
+    public static void main(String[] args)
+    { //method called main
+	    var dozer = new Bulldozer();
+	    dozer.color = "yellow";
+	    dozer.size = "large";
+
+        System.out.println("Bulldozer is: + dozer.color");
+    }
+    public class Bulldozer{
+        public string color(get; set: )
+        public string size(get; set:)
+    }
+}
--- Sec5Lec28ii/src/com/faithefm/Main.java
+++ Sec5Lec28ii/src/com/faithefm/Main.java
@@ -3,7 +3,19 @@ package com.faithefm;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	boolean gameOver = true;
+	int score = 800;
+	int levelCompleted = 5;
+	int bonus = 100;
+
+	int highScore = calculateScore(gameOver, score, levelCompleted, bonus); //assigning a variable to a method
+        System.out.println("Your final score was " + highScore);
+
+	score = 10000;
+	levelCompleted = 8;
+	bonus = 200;
+
+	calculateScore(gameOver, score, levelCompleted, bonus);
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
